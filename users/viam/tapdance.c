0480c277b87e6246f16d9bc5105b2b63ddafb495
roninyojimbo3 <roninyojimbo3@gmail.com>
2026-06-15 00:37:13 -0700

    Update tapdance.c

--- users/viam/tapdance.c
+++ users/viam/tapdance.c
@@ -18,20 +18,24 @@
         }
     #endif // BETTER_DRAGSCROLL_TAPDANCE
 
-    void nano_click_finished(tap_dance_state_t *state, void *user_data) {
-        if (state->count == 1) {
-            tap_code(X_MS_BTN1);   // single tap = left click
-        } else {
-            tap_code(X_MS_BTN2);   // double tap or more = right click
+    #if defined(NANO2_CLICK_TAPDANCE)
+        void nano_click_finished(tap_dance_state_t *state, void *user_data) {
+            if (state->count == 1) {
+                tap_code(MS_BTN1);   // single tap = left click
+            } else {
+                tap_code(MS_BTN2);   // double tap or more = right click
+            }
         }
-    }
+    #endif // NANO2_CLICK_TAPDANCE
 
     tap_dance_action_t tap_dance_actions[] = {
         #if defined(BETTER_DRAGSCROLL_TAPDANCE) || defined(BETTER_DRAGSCROLL_TAPDANCE_FN_ADVANCE)
             [TD_DRAGSCROLL] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dragscrolltd_fin, dragscrolltd_rst),
         #endif // BETTER_DRAGSCROLL_TAPDANCE
 
-        [TD_NANO_CLICK] = ACTION_TAP_DANCE_FN(nano_click_finished),
+        #if defined(NANO2_CLICK_TAPDANCE)
+            [TD_NANO_CLICK] = ACTION_TAP_DANCE_FN(nano_click_finished),
+        #endif // NANO2_CLICK_TAPDANCE
     };
 
 #endif // TAP_DANCE_ENABLE
