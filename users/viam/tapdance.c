09197c1d037b4d0732985c033038c9dc87c08902
roninyojimbo3 <roninyojimbo3@gmail.com>
2026-06-15 00:19:03 -0700

    Update tapdance.c

--- users/viam/tapdance.c
+++ users/viam/tapdance.c
@@ -1,27 +1,37 @@
 #if defined(TAP_DANCE_ENABLE)
 
-  #if defined(BETTER_DRAGSCROLL_TAPDANCE)
-    #include "better_dragscroll.h"
-    void dragscrolltd_fin(tap_dance_state_t *state, void *user_data){
-      dprintf("fin; pressed:%d count:%d \n", state->pressed, state->count);
-      if(state->count >= 2){
-        better_dragscroll_toggle(true);
-      }
-      else{
-        better_dragscroll_momentary(true);
-      }
-    }
-    void dragscrolltd_rst(tap_dance_state_t *state, void *user_data){
-      if(state->count < 2){
-        better_dragscroll_momentary(false);
-      }
-    }
-  #endif // BETTER_DRAGSCROLL_TAPDANCE
-
-  tap_dance_action_t tap_dance_actions[] = {
-    #if defined(BETTER_DRAGSCROLL_TAPDANCE) || defined(BETTER_DRAGSCROLL_TAPDANCE)
-      [TD_DRAGSCROLL] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dragscrolltd_fin, dragscrolltd_rst),
+    #if defined(BETTER_DRAGSCROLL_TAPDANCE)
+        #include "better_dragscroll.h"
+        void dragscrolltd_fin(tap_dance_state_t *state, void *user_data){
+            dprintf("fin; pressed:%d count:%d \n", state->pressed, state->count);
+            if(state->count >= 2){
+                better_dragscroll_toggle(true);
+            }
+            else{
+                better_dragscroll_momentary(true);
+            }
+        }
+        void dragscrolltd_rst(tap_dance_state_t *state, void *user_data){
+            if(state->count < 2){
+                better_dragscroll_momentary(false);
+            }
+        }
     #endif // BETTER_DRAGSCROLL_TAPDANCE
 
-  };
+    void nano_click_finished(tap_dance_state_t *state, void *user_data) {
+        if (state->count == 1) {
+            tap_code(X_MS_BTN1);   // single tap = left click
+        } else {
+            tap_code(X_MS_BTN2);   // double tap or more = right click
+        }
+    }
+
+    tap_dance_action_t tap_dance_actions[] = {
+        #if defined(BETTER_DRAGSCROLL_TAPDANCE) || defined(BETTER_DRAGSCROLL_TAPDANCE_FN_ADVANCE)
+            [TD_DRAGSCROLL] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dragscrolltd_fin, dragscrolltd_rst),
+        #endif // BETTER_DRAGSCROLL_TAPDANCE
+
+        [TD_NANO_CLICK] = ACTION_TAP_DANCE_FN(nano_click_finished),
+    };
+
 #endif // TAP_DANCE_ENABLE
